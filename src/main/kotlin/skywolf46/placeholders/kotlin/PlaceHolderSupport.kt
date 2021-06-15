4b0c6822951037e3d93b5a926afe16f7effa8d31
milkyway0308 <skywolf461@gmail.com>
2021-06-15 19:56:59 +0900

    1.6.0 - Added kotlin support

--- src/main/kotlin/skywolf46/placeholders/kotlin/PlaceHolderSupport.kt
+++ src/main/kotlin/skywolf46/placeholders/kotlin/PlaceHolderSupport.kt
@@ -3,6 +3,7 @@ package skywolf46.placeholders.kotlin
 import org.bukkit.plugin.java.JavaPlugin
 import skywolf46.messagereplacer.MessageReplacer
 import java.io.File
+import kotlin.reflect.KClass
 
 class PlaceHolderSupport {
 }
@@ -13,12 +14,21 @@ fun <T> T.registerStorage(fx: File) = MessageReplacer.register(this!!::class.jav
 
 inline fun <reified T : Enum<Any>> T.toMessage() = MessageReplacer.get(T::class.java)[this]
 
-fun <T : Enum<Any>> Class<out T>.registerAll(plugin: JavaPlugin) {
-    registerAll(File(plugin.dataFolder, "messages.yml"))
+fun KClass<*>.registerIfEnum(plugin: JavaPlugin) {
+    java.registerIfEnum(plugin)
+}
+
+fun KClass<*>.registerIfEnum(file: File) {
+    java.registerIfEnum(file)
+}
+
+fun Class<*>.registerIfEnum(plugin: JavaPlugin) {
+    registerIfEnum(File(plugin.dataFolder, "messages.yml"))
 }
 
 
-fun <T : Enum<Any>> Class<out T>.registerAll(file: File) {
-    MessageReplacer.register(this, file).registerIfNotExists(getMethod("values").invoke(null) as Array<Enum<*>>)
-        .save(file)
+fun Class<*>.registerIfEnum(file: File) {
+    if (Enum::class.java.isAssignableFrom(this))
+        MessageReplacer.register(this, file).registerIfNotExists(getMethod("values").invoke(null) as Array<Enum<*>>)
+            .save(file)
 }
