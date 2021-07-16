930acb93db22e32ba61ee6549fccbf54c103e9b1
milkyway0308 <skywolf461@gmail.com>
2021-07-17 07:47:50 +0900

    2.0.0 - Added multiple prefixed holder test

--- src/test/kotlin/skywolf46/placeholderkotlin/test/StringParseTest.kt
+++ src/test/kotlin/skywolf46/placeholderkotlin/test/StringParseTest.kt
@@ -50,4 +50,25 @@ class StringParseTest {
                 .parse(ArgumentStorage())
         )
     }
+
+    @Test
+    fun multiplePrefixedPlaceHolderParseTest() {
+        val targetString =
+            "test, <test2>[test2]<test2>[test2],asdf,<test><test><Test> <Test2> {test3}<test3}[test3]<test3>.<test> <Test3><test2>!"
+        storage.registerPlaceholder("<", ">", "test", TestPlaceHolderFirst::class.java)
+        storage.registerPlaceholder("[", "]", "test2", TestPlaceHolderSecond::class.java)
+        storage.registerPlaceholder("{", "}", "test3", TestPlaceHolderThird::class.java)
+        assertEquals(
+            targetString
+                .replace("<test>", "Test")
+                .replace("[test2]", "Test2")
+                .replace("{test3}", "Test3"),
+            parser.analyze(arrayOf(),
+                ArgumentStorage(),
+                targetString)
+                .parse(ArgumentStorage()).apply {
+                    println("Final: $this")
+                }
+        )
+    }
 }
